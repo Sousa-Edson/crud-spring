b85012230abf1e02f9b1f946c0f91dca5c9ccf1d
Edson Sousa <edson3711.es@gmail.com>
2024-01-28 11:51:26 -0300

    buscando por id

--- src/main/java/com/edson/crudspring/controller/CourseController.java
+++ src/main/java/com/edson/crudspring/controller/CourseController.java
@@ -4,6 +4,7 @@ import com.edson.crudspring.model.Course;
 import com.edson.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,14 +21,15 @@ public class CourseController {
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable("id") Long id){
+      return courseRepository.findById(id)
+              .map(record -> ResponseEntity.ok().body(record))
+              .orElse(ResponseEntity.notFound().build());
+    }
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
-//        @PostMapping
-//        public ResponseEntity<Course> create(@RequestBody Course course) {
-//            return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
-//        }
     }
 }
